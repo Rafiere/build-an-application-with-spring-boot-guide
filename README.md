31828227ec2dc4ef6435e7110f8620c62db248bc
Rafa <teste@teste.com>
2021-10-17 19:41:30 -0300

    Primeira modificação do projeto

---
+++ README.md
@@ -0,0 +1,4 @@
+Projeto desenvolvido com base no tutorial do site "Getting Started", do Spring.
+
+Link do Tutorial: https://spring.io/guides/gs/spring-boot/
+
---
+++ src/main/java/br/com/projetobasico/buildinganapplicationwithspringboot/BuildingAnApplicationWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetobasico.buildinganapplicationwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingAnApplicationWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingAnApplicationWithSpringBootApplication.class, args);
+	}
+
+}
